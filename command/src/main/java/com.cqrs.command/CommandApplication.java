807bd66284204aa2a749cf039076602ee22912be
jhbang <jhbang@nhnsoft.com>
2022-01-12 14:58:12 +0900

    initial

---
+++ command/src/main/java/com.cqrs.command/CommandApplication.java
@@ -0,0 +1,8 @@
+package com.cqrs.command;
+
+@SpringBootApplication
+public class CommandApplication {
+    public static void main(String[] args){
+        SpringApplication.run(CommandApplication.class, args);
+    }
+}
